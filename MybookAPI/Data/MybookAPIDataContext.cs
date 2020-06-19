d7ccacdfc2d77ef4b030dce8204a2e5a95a2255e
devon1910 <62015545+devon1910@users.noreply.github.com>
2020-06-19 11:41:55 +0100

    Add files via upload

---
+++ MybookAPI/Data/MybookAPIDataContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MybookAPI.Entities;
+
+namespace MybookAPI.Data
+{
+    public class MybookAPIDataContext : DbContext
+    {
+        public MybookAPIDataContext(DbContextOptions<MybookAPIDataContext> options) : base(options)
+        {
+        }
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Genre> Genres { get; set; }
+        public virtual DbSet<Book> Books { get; set; }
+        public virtual DbSet<Category> Category { get; set; }
+        public virtual DbSet<Publisher> Publishers { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+    }
+}
